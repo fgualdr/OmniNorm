85fcbef366c27a9ddcac255248915650711278c3
fgualdr <francesco.gualdrini@gmail.com>
2022-11-10 09:59:21 +0100

    second commit

--- R/pair_fit.R
+++ R/pair_fit.R
@@ -1,6 +1,31 @@
-# Fitting theorugh mixed Skew normal 
-pair_fit <- function( ll ){
-  smsnmean <- function(mu, sigma2, shape){
+#' pair_fit execute pair wise fitting of Skewed normal on the freq. distribution of the ratios.
+#'
+#' @param ll List composed by: "ratio" for the selected pair, "n_pop" i.e. n° of expected populations,
+#'          "sigma_times" n° of sigmas to select invariant observations, "dist_family" distribution famly see mixsmsn, ...
+#'
+#' @returns Return EM algorithm output for FM-SMSN distributions with additional fields: n_pop,means,modes,fre  q,freq_dist,sigma_norm
+#' @export
+#'
+#' @import mixsmsn
+#' @import graphics
+#' @import grDevices
+#' @import methods
+#' @import stats
+#' @import utils
+#'
+#' @examples
+#' \dontrun{
+#' # Typical usage
+#' ll <- list(
+#'   "ratio" = ratio, "n_pop" = n_pop_reference,
+#'   "sigma_times" = sigma_times, "dist_family" = dist_family, "index" = x
+#' )
+#' model <- pair_fit(ll)
+#' }
+#'
+pair_fit <- function(ll) {
+  # Import/create dedicated functions
+  smsnmean <- function(mu, sigma2, shape) {
     xi <- mu
     omega <- sqrt(sigma2)
     alpha <- shape
@@ -9,76 +34,86 @@ pair_fit <- function( ll ){
     mean <- xi + omega * delta * C
     mean
   }
-  ratio=ll[["ratio"]]
-  sigma_times = ll[["sigma_times"]]
-  family = ll[["dist_family"]]
-  n_pop = ll[["n_pop"]]
+  dSN <- utils::getFromNamespace("dSN", "mixsmsn")
+  d.mixedSN <- utils::getFromNamespace("d.mixedSN", "mixsmsn")
 
-  if(is.null(sigma_times)){sigma_times=1}
+  ratio <- ll[["ratio"]]
+  sigma_times <- ll[["sigma_times"]]
+  family <- ll[["dist_family"]]
+  n_pop <- ll[["n_pop"]]
 
-  if(is.null(n_pop)){
+  if (is.null(sigma_times)) {
+    sigma_times <- 1
+  }
+
+  if (is.null(n_pop)) {
     # Discover best n° of peaks:
     model_search <- mixsmsn::smsn.search(ratio, 3,
-                  g.min = 1, g.max = 3,
-                  family = family, criteria = "bic",
-                  error = 0.00001, iter.max = 100,
-                  calc.im = FALSE, uni.Gama = FALSE, kmeans.param = NULL)
-    n_pop = length(unique(model_search$best.model$group))
+      g.min = 1, g.max = 3,
+      family = family, criteria = "bic",
+      error = 0.00001, iter.max = 100,
+      calc.im = FALSE, uni.Gama = FALSE, kmeans.param = NULL
+    )
+    n_pop <- length(unique(model_search$best.model$group))
   }
 
   # Compute the mixed Skewed model
-  model <- mixsmsn::smsn.mix( ratio, 
-                              nu = 3 , 
-                              g = n_pop, 
-                              get.init = TRUE, 
-                              criteria = TRUE,
-                              iter.max = 1000,
-                              error = 0.00001, 
-                              group = TRUE, 
-                              family = family, 
-                              calc.im = FALSE)
-  model$n_pop = n_pop
-  xx=seq( min(ratio), max(ratio), (max(ratio) - min(ratio))/(length(ratio)*100) )
-	dens <- matrix(NA_real_, ncol = n_pop, nrow = length(xx))
-	means <- vector("numeric", n_pop)
-	modes <- vector("numeric", n_pop)
-	dens.max <- vector("numeric", n_pop)  
+  model <- mixsmsn::smsn.mix(ratio,
+    nu = 3,
+    g = n_pop,
+    get.init = TRUE,
+    criteria = TRUE,
+    iter.max = 1000,
+    error = 0.00001,
+    group = TRUE,
+    family = family,
+    calc.im = FALSE
+  )
+  model$n_pop <- n_pop
+  xx <- seq(min(ratio), max(ratio), (max(ratio) - min(ratio)) / (length(ratio) * 100))
+  dens <- matrix(NA_real_, ncol = n_pop, nrow = length(xx))
+  means <- vector("numeric", n_pop)
+  modes <- vector("numeric", n_pop)
+  dens.max <- vector("numeric", n_pop)
   n_calls <- vector("numeric", n_pop)
   sigmas <- vector("numeric", n_pop)
-  for( j in 1 : n_pop){
-    dens[,j] <- mixsmsn:::dSN(xx, model$mu[j], model$sigma2[j], model$shape[j])
+  for (j in 1:n_pop) {
+    dens[, j] <- dSN(xx, model$mu[j], model$sigma2[j], model$shape[j])
     means[j] <- smsnmean(model$mu[j], model$sigma2[j], model$shape[j])
-    dens.max[j] <- which.max(dens[,j])
+    dens.max[j] <- which.max(dens[, j])
     modes[j] <- xx[dens.max[j]]
-    sk = ratio[model$group == j ]
-    f <- fitdistrplus::fitdist( data=sk, distr="norm" ,keepdata = FALSE,discrete=FALSE,fix.arg=list("mean"= modes[j] ))
-    sigmas[j] <-  f$estimate["sd"]
+    sk <- ratio[model$group == j]
+    f <- fitdistrplus::fitdist(data = sk, distr = "norm", keepdata = FALSE, discrete = FALSE, fix.arg = list("mean" = modes[j]))
+    sigmas[j] <- f$estimate["sd"]
   }
-  global_dens = mixsmsn:::d.mixedSN(xx, model$pii, model$mu, model$sigma2, model$shape)
-  freq_sum = xx[which.max(global_dens)]
+  global_dens <- d.mixedSN(xx, model$pii, model$mu, model$sigma2, model$shape)
+  freq_sum <- xx[which.max(global_dens)]
   # Decorate the model
   model$means <- means
   model$modes <- modes
-  model$freq = apply(dens,2,max)
-  model$freq_dist = abs(modes - freq_sum)
-  model$sigma_norm = sigmas
+  model$freq <- apply(dens, 2, max)
+  model$freq_dist <- abs(modes - freq_sum)
+  model$sigma_norm <- sigmas
   # Select the reference population based on score: i.e. highest frequency and smaller error:
-  score1 = (model$freq )/(max(model$freq)) # higher the better # min max(model$freq)
-  score2 =  max(model$freq_dist)/ model$freq_dist # smaller the better
-  score2 = score2/max(score2)
-  if(model$freq_dist == 0){score2=1}
-  score3 =  max(model$sigma_norm)/model$sigma_norm # smaller the better
-  score3 = score3/max(score3)
-  model$score =  score1 + score2 + score3 
-  # we fix to the mode to get the sd of the best fit normal
-  interval = c(   model$means[which.max(model$score)], 
-                  model$modes[which.max(model$score)], 
-                  model$modes[which.max(model$score)], 
-                  model$sigma_norm[which.max(model$score)], 
-                  model$modes[which.max(model$score)] - (sigma_times *  model$sigma_norm[which.max(model$score)]),
-                  model$modes[which.max(model$score)] + (sigma_times *  model$sigma_norm[which.max(model$score)])
-                  )
-    names(interval) = c("mean_sn","mode_sn","mean_g","sd","lb","ub")
-    model$interval = interval
-    return(model)
+  score1 <- (model$freq) / (max(model$freq)) # higher the better # min max(model$freq)
+  score2 <- max(model$freq_dist) / model$freq_dist # smaller the better
+  score2 <- score2 / max(score2)
+  if (model$freq_dist == 0) {
+    score2 <- 1
   }
+  score3 <- max(model$sigma_norm) / model$sigma_norm # smaller the better
+  score3 <- score3 / max(score3)
+  model$score <- score1 + score2 + score3
+  # we fix to the mode to get the sd of the best fit normal
+  interval <- c(
+    model$means[which.max(model$score)],
+    model$modes[which.max(model$score)],
+    model$modes[which.max(model$score)],
+    model$sigma_norm[which.max(model$score)],
+    model$modes[which.max(model$score)] - (sigma_times * model$sigma_norm[which.max(model$score)]),
+    model$modes[which.max(model$score)] + (sigma_times * model$sigma_norm[which.max(model$score)])
+  )
+  names(interval) <- c("mean_sn", "mode_sn", "mean_g", "sd", "lb", "ub")
+  model$interval <- interval
+  return(model)
+}
--- R/plot_pair_model.R
+++ R/plot_pair_model.R
@@ -1,19 +1,31 @@
-
-plot_pair_model <- function(ll){
-  model=ll[["model"]]
-  mat=ll[["mat_append"]]
-  sample_name=ll[["sample_name"]]
-  reference_name=ll[["reference_name"]]
-  path=ll[["saving_path"]]
-  n_pop=ll[["n_pop"]]
-  # Save_pair plots:
-  if(!is.null(path)){
-    save = paste0(path,"/Norm_pair_plots/")
-    dir.create(save)
-  }else{
-    stop("ERROR: to plot the Normalisation pair plots a path to a general output folder is required")
-  }
-  smsnmean <- function(mu, sigma2, shape){
+#' plot_pair_model Plots fitted distributions to the ratios.
+#'
+#' @param ll List composed by: "ratio" for the selected pair, "n_pop" i.e. n° of expected populations,
+#'          "sigma_times" n° of sigmas to select invariant observations, "dist_family" distribution famly see mixsmsn, ...
+#'
+#' @returns Returns automatically pdf saved within "saving_path"/Norm_pair_plots/ of the fitting and QC
+#' @export
+#'
+#' @import mixsmsn
+#' @import graphics
+#' @import grDevices
+#' @import methods
+#' @import stats
+#' @import utils
+#'
+#' @examples
+#' \dontrun{
+#' # Typical usage
+#' ll <- list(
+#'   "ratio" = ratio, "n_pop" = n_pop_reference,
+#'   "sigma_times" = sigma_times, "dist_family" = dist_family, "index" = x
+#' )
+#' plot_pair_model(ll)
+#' }
+#'
+plot_pair_model <- function(ll) {
+  # Import generate functions:
+  smsnmean <- function(mu, sigma2, shape) {
     xi <- mu
     omega <- sqrt(sigma2)
     alpha <- shape
@@ -22,64 +34,80 @@ plot_pair_model <- function(ll){
     mean <- xi + omega * delta * C
     mean
   }
-  ratio = log( mat[,reference_name] / mat[,sample_name] )
+  dSN <- utils::getFromNamespace("dSN", "mixsmsn")
+
+  model <- ll[["model"]]
+  mat <- ll[["mat_append"]]
+  sample_name <- ll[["sample_name"]]
+  reference_name <- ll[["reference_name"]]
+  path <- ll[["saving_path"]]
+  n_pop <- ll[["n_pop"]]
+  # Save_pair plots:
+  if (!is.null(path)) {
+    save <- paste0(path, "/Norm_pair_plots/")
+    dir.create(save)
+  } else {
+    stop("ERROR: to plot the Normalisation pair plots a path to a general output folder is required")
+  }
+
+  ratio <- log(mat[, reference_name] / mat[, sample_name])
   # Only consider finite values
-  ratio = ratio[is.finite(ratio)]
-  xx=seq( min(ratio), max(ratio), (max(ratio) - min(ratio))/(length(ratio)*100) )
+  ratio <- ratio[is.finite(ratio)]
+  xx <- seq(min(ratio), max(ratio), (max(ratio) - min(ratio)) / (length(ratio) * 100))
   # We produce grey lines for all but the selected population:
   # To select the reference population we can two main criteria plus an additional:
   # 1) Small error
   # 2) comprise the largest population
-  score = model$score
+  score <- model$score
   # Define colours e.g. reference population in "red"
-	myColors <- rep("black",n_pop)
-  myColors[which.max(score)] = "red"
-  samp_apirs = paste0("Sample_",sample_name,"_Reference_",reference_name,".pdf")
-  nm = paste0(save,"/",samp_apirs)
+  myColors <- rep("black", n_pop)
+  myColors[which.max(score)] <- "red"
+  samp_apirs <- paste0("Sample_", sample_name, "_Reference_", reference_name, ".pdf")
+  nm <- paste0(save, "/", samp_apirs)
   pdf(nm)
-  par(mar=c(2, 2, 2, 2))
-  layout(matrix(c(1,1,1,1,1,1,1,1,2,2,3,3,2,2,3,3), 4, 4, byrow = TRUE))
-  #par(mfrow = c(1, 3), pty="s")
-    # Model fitting plot
-    mixsmsn::mix.hist(ratio, model,col.hist="grey",border="white",cex=0.5, cex.lab=.5, cex.axis=.5, cex.main=.5, cex.sub=.5) 
-    mixsmsn::mix.lines(ratio, model, cex=0.5, cex.lab=.5, cex.axis=.5, cex.main=.5, cex.sub=.5,col = "blue")
-    dens <- matrix(NA_real_, ncol = n_pop, nrow = length(xx))
-    dens.list <- vector("list", n_pop)
-    means <- vector("numeric", n_pop)
-    modes <- vector("numeric", n_pop)
-    dens.max <- vector("numeric", n_pop)  
-    n_calls <- vector("numeric", n_pop)
-    for( j in 1 : n_pop){
-      dens<- mixsmsn:::dSN(xx, model$mu[j], model$sigma2[j], model$shape[j])
-      dens.list[[j]] <- data.frame(x = xx, y = dens)
-      means[j] <- smsnmean(model$mu[j], model$sigma2[j], model$shape[j])
-      dens.max[j] <- which.max(dens.list[[j]]$y)
-      modes[j] <- dens.list[[j]]$x[dens.max[j]]
-      lines(xx, model$pii[j] * dens, type = 'l', col = myColors[j], lwd = 1)
-      lines(segments( means[j], 0, means[j], model$pii[j] * mixsmsn:::dSN(model$means[j], model$mu[j], model$sigma2[j], model$shape[j]), lty = 2, lwd = 1, col = myColors[j]))
-      lines(segments(modes[j], 0, modes[j], model$pii[j] * dens[dens.max], col = myColors[j], lwd = 1))
-    }
-    # Scatter with selected population
-    mmax = as.numeric(c(mat[,reference_name],mat[,sample_name]))
-    mmax = max(mmax[is.finite(mmax)])
-    mmin = as.numeric(c(mat[,reference_name],mat[,sample_name]))
-    mmin = mmin[is.finite(mmin)]
-    mmin = min(mmin[mmin != 0])
-    ##
-    ww <- which( log( mat[,reference_name] / mat[,sample_name] ) >= model$interval["lb"] & 
-                    log( mat[,reference_name] / mat[,sample_name] ) <= model$interval["ub"] )
-    # Ori mat
-    plot(mat[,reference_name],mat[,sample_name],xlab="Average reference", ylab="Selected Sample",cex=0.5, cex.lab=.5, cex.axis=.5, cex.main=.5, cex.sub=.5,log ="xy",xlim=c(mmin,mmax),ylim=c(mmin,mmax),pch=20,asp=1)
-    points(mat[ww,reference_name],mat[ww,sample_name] ,log ="xy",xlim=c(mmin,mmax),ylim=c(mmin,mmax),cex=0.5,col="red")
-    abline(coef=c(0,1),col="red")
-    abline(v=100)
-    abline(h=100)
+  par(mar = c(2, 2, 2, 2))
+  layout(matrix(c(1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3), 4, 4, byrow = TRUE))
+  # par(mfrow = c(1, 3), pty="s")
+  # Model fitting plot
+  mixsmsn::mix.hist(ratio, model, col.hist = "grey", border = "white", cex = 0.5, cex.lab = .5, cex.axis = .5, cex.main = .5, cex.sub = .5)
+  mixsmsn::mix.lines(ratio, model, cex = 0.5, cex.lab = .5, cex.axis = .5, cex.main = .5, cex.sub = .5, col = "blue")
+  dens <- matrix(NA_real_, ncol = n_pop, nrow = length(xx))
+  dens.list <- vector("list", n_pop)
+  means <- vector("numeric", n_pop)
+  modes <- vector("numeric", n_pop)
+  dens.max <- vector("numeric", n_pop)
+  n_calls <- vector("numeric", n_pop)
+  for (j in 1:n_pop) {
+    dens <- dSN(xx, model$mu[j], model$sigma2[j], model$shape[j])
+    dens.list[[j]] <- data.frame(x = xx, y = dens)
+    means[j] <- smsnmean(model$mu[j], model$sigma2[j], model$shape[j])
+    dens.max[j] <- which.max(dens.list[[j]]$y)
+    modes[j] <- dens.list[[j]]$x[dens.max[j]]
+    lines(xx, model$pii[j] * dens, type = "l", col = myColors[j], lwd = 1)
+    lines(segments(means[j], 0, means[j], model$pii[j] * dSN(model$means[j], model$mu[j], model$sigma2[j], model$shape[j]), lty = 2, lwd = 1, col = myColors[j]))
+    lines(segments(modes[j], 0, modes[j], model$pii[j] * dens[dens.max], col = myColors[j], lwd = 1))
+  }
+  # Scatter with selected population
+  mmax <- as.numeric(c(mat[, reference_name], mat[, sample_name]))
+  mmax <- max(mmax[is.finite(mmax)])
+  mmin <- as.numeric(c(mat[, reference_name], mat[, sample_name]))
+  mmin <- mmin[is.finite(mmin)]
+  mmin <- min(mmin[mmin != 0])
+  ##
+  ww <- which(log(mat[, reference_name] / mat[, sample_name]) >= model$interval["lb"] &
+    log(mat[, reference_name] / mat[, sample_name]) <= model$interval["ub"])
+  # Ori mat
+  plot(mat[, reference_name], mat[, sample_name], xlab = "Average reference", ylab = "Selected Sample", cex = 0.5, cex.lab = .5, cex.axis = .5, cex.main = .5, cex.sub = .5, log = "xy", xlim = c(mmin, mmax), ylim = c(mmin, mmax), pch = 20, asp = 1)
+  points(mat[ww, reference_name], mat[ww, sample_name], log = "xy", xlim = c(mmin, mmax), ylim = c(mmin, mmax), cex = 0.5, col = "red")
+  abline(coef = c(0, 1), col = "red")
+  abline(v = 100)
+  abline(h = 100)
 
-    # Scaled mat
-    plot(mat[,reference_name], mat[,sample_name] * (exp(model$interval["mean_g"])) ,xlab="Average reference", ylab="Selected Sample",cex=0.5, cex.lab=.5, cex.axis=.5, cex.main=.5, cex.sub=.5,log ="xy",xlim=c(mmin,mmax),ylim=c(mmin,mmax),pch=20,asp=1)
-    abline(coef=c(0,1),col="red")
-    abline(v=100)
-    abline(h=100)
+  # Scaled mat
+  plot(mat[, reference_name], mat[, sample_name] * (exp(model$interval["mean_g"])), xlab = "Average reference", ylab = "Selected Sample", cex = 0.5, cex.lab = .5, cex.axis = .5, cex.main = .5, cex.sub = .5, log = "xy", xlim = c(mmin, mmax), ylim = c(mmin, mmax), pch = 20, asp = 1)
+  abline(coef = c(0, 1), col = "red")
+  abline(v = 100)
+  abline(h = 100)
 
   dev.off()
 }
